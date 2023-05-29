131121d755b925cced860f9de5904b25cc511ed3
@thiagosilva <48137320+kaleno@users.noreply.github.com>
2023-05-28 21:52:12 -0300

    Delete [Questionário] Iteração/[Questionário] Iteração directory

--- [Questionário] Iteração/[Questionário] Iteração/Questão-1.py
+++
@@ -1,7 +0,0 @@
-cont = 0
-texto = ""
-while texto != "Fim":
-    texto = input()
-    cont = cont + 1
-print(cont-1)
-    
--- [Questionário] Iteração/[Questionário] Iteração/Questão-7.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-cont= 0
-while cont <= n: #<- n+1/21 laços!
-    print(cont,end=" ")
-    cont = cont+21
-
